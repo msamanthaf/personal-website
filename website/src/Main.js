3d8854ae46ab83469c3c84fd014e2dd4ea391b44
Flayre <mariesamantha.f@gmail.com>
2023-05-11 23:02:15 +0700

    Fixed header

--- website/src/Main.js
+++ website/src/Main.js
@@ -3,12 +3,15 @@ import './Main.css';
 const Web = () => {
   return (
 <div className="main">
-<header className="header">
- <img className="logo" src="Icon.png" alt="Logo" />
- <button className="header-buttons">About</button>
- <button className="header-buttons">Portfolio</button>
- <button className="header-buttons">Contact</button>
- </header>
+<header>
+<img src="Icon.png" alt="Logo" className="logo" />
+  <nav className="header-nav">
+      <a href='#about' className="header-button">Profile</a>
+      <a href='#portfolio' className="header-button">Portfolio</a>
+      <a href='#contact' className="header-button">Contact</a>
+  </nav>
+</header>
+
 </div>
   );
 }
