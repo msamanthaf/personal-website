85c45769b10e376d40c1c08da21aa31a84aaa5ea
Flayre <mariesamantha.f@gmail.com>
2023-05-12 03:12:30 +0700

    Completely fixed scrolling issue on reload

--- website/src/Scroll.js
+++ website/src/Scroll.js
@@ -1,18 +1,24 @@
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('nav a');
-
-window.onscroll = () => {
-     sections.forEach(sec => {
-         let top = window.scrollY;
-         let offset = sec.offsetTop - 150;
-         let height = sec.offsetHeight;
-         let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height) {
-             navLinks.forEach(links => {
-                 links.classList.remove('active');
-                 document.querySelector('nav a[href*=' + id + ']').classList.add('active');
-             });
-         };
-    });
-};
+function init() {
+    let sections = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('nav a');
+  
+    window.onscroll = () => {
+      sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+  
+        if (top >= offset && top < offset + height) {
+          navLinks.forEach(links => {
+            links.classList.remove('active');
+            document.querySelector('nav a[href*=' + id + ']').classList.add('active');
+          });
+        };
+      });
+    };
+  }
+  
+  window.onload = () => {
+    init();
+  };
