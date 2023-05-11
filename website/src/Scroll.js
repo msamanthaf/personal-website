b5ec116d52c1f0e7dc0a4dcdc40b79324d38dad0
Flayre <mariesamantha.f@gmail.com>
2023-05-12 03:03:45 +0700

    Fixed scrolling bug

--- website/src/Scroll.js
+++ website/src/Scroll.js
@@ -4,14 +4,14 @@ let navLinks = document.querySelectorAll('nav a');
 window.onscroll = () => {
      sections.forEach(sec => {
          let top = window.scrollY;
-         let offset = sec.offsetTop;
+         let offset = sec.offsetTop - 150;
          let height = sec.offsetHeight;
          let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height) {
              navLinks.forEach(links => {
                  links.classList.remove('active');
-                 document.querySelector('nav a[href="#' + id + '"]').classList.add('active');
+                 document.querySelector('nav a[href*=' + id + ']').classList.add('active');
              });
          };
     });
