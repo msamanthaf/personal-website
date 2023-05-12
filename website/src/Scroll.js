cc1b0d550d6e13651c86c3c54fdb02503d1472d8
Flayre <mariesamantha.f@gmail.com>
2023-05-12 16:44:19 +0700

    Added background transition to header

--- website/src/Scroll.js
+++ website/src/Scroll.js
@@ -1,11 +1,12 @@
 function init() {
     let sections = document.querySelectorAll('section');
     let navLinks = document.querySelectorAll('nav a');
-  
+    let header = document.querySelector('header');
+
     window.onscroll = () => {
       sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop;
+        let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
   
@@ -14,6 +15,11 @@ function init() {
             links.classList.remove('active');
             document.querySelector('nav a[href*=' + id + ']').classList.add('active');
           });
+          if (id !== "profile") {
+            header.style.backgroundColor = "#15131d";
+          } else {
+            header.style.backgroundColor = "transparent";
+          }
         };
       });
     };
