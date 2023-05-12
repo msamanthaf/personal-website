e00f7346a7db0dfda45a53f0ad290f8d1eb21722
Flayre <mariesamantha.f@gmail.com>
2023-05-12 19:21:29 +0700

    Added profile body and animation effects

--- website/src/Main.js
+++ website/src/Main.js
@@ -14,7 +14,15 @@ const Web = () => {
   </nav>
 </header>
 
-<section id="profile">Profile</section>
+<section id="profile">
+  <div class="text-box">
+    <p>Hey👋, I'm</p>
+    <h1>MARIE</h1> 
+    <h2><h3>SAM</h3> ANTHA</h2>
+    <a href='#profile'>Explore What I Do <span>&#x27F6;</span></a>
+    <a href='#portfolio'>Connect With Me 🙌</a>
+  </div>
+</section>
 <section id="portfolio">Portfolio</section>
 <section id="contact">Contact</section>
 <script src = "Scroll.js"></script>
