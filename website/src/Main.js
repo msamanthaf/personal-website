5f3c47ae15c85bbc84f9a987b8affe73aa9f5788
Flayre <mariesamantha.f@gmail.com>
2023-05-23 04:26:51 +0700

    Added slider button and images

--- website/src/Main.js
+++ website/src/Main.js
@@ -5,7 +5,7 @@ const Web = () => {
   return (
 <div className="main">
 <header>
-<a href='#profile'><img src="Icon.png" className="logo" /></a>
+<a href='#profile'><img src="images/Icon.png" className="logo" /></a>
   <nav className="header-nav">
       <a href='#profile' class="active">Profile</a>
       <a href='#portfolio'>Portfolio</a>
@@ -39,9 +39,9 @@ const Web = () => {
     </div>
   </div>
   <div id='icons'>
-    <a href='https://drive.google.com/file/d/18LWYla920C4FaV_P4nnHr5-viNb-3aoT/view?usp=sharing'><img src="resumeicon.png" className="resume" /></a>
-    <a href='https://www.linkedin.com/in/marie-samantha-fidelia-82a3151b0/'><img src="linkedin.png" className="linkedin" /></a>
-    <a href='https://github.com/msamanthaf'><img src="github.png" className="github" /></a>
+    <a href='https://drive.google.com/file/d/18LWYla920C4FaV_P4nnHr5-viNb-3aoT/view?usp=sharing'><img src="images/resumeicon.png" className="resume" /></a>
+    <a href='https://www.linkedin.com/in/marie-samantha-fidelia-82a3151b0/'><img src="images/linkedin.png" className="linkedin" /></a>
+    <a href='https://github.com/msamanthaf'><img src="images/github.png" className="github" /></a>
   </div>
 </section>
 
@@ -52,11 +52,15 @@ const Web = () => {
         <span class="slider-span" id="slider-span1"></span>
         <span class="slider-span" id="slider-span2"></span>
         <span class="slider-span" id="slider-span3"></span>
+        <span class="slider-span" id="slider-span4"></span>
+        <span class="slider-span" id="slider-span5"></span>
 
         <div class="image-slider">
             <div class="slides-div" id="slide-1">
-                <img src="images/headset.jpg" alt="" class="img" id="img1"></img>
-                <a href="#slider-span1" class="button" id="button-1"></a>
+                <img src="images/cardimg.png" alt="" class="img" id="img1"></img>
+                <a href="#slider-span1" class="button" id="button-1">
+                <a href="https://github.com/msamanthaf" class="view-project" id="view-button"><p4 id="view-text">View &#x27F6;</p4></a>
+                </a>
             </div>
             <div class="slides-div" id="slide-2">
                 <img src="images/headset.jpg" alt="" class="img" id="img2"></img>
@@ -65,6 +69,14 @@ const Web = () => {
             <div class="slides-div" id="slide-3">
                 <img src="images/headset.jpg" alt="" class="img" id="img3"></img>
                 <a href="#slider-span3" class="button" id="button-3"></a>
+            </div>
+            <div class="slides-div" id="slide-4">
+                <img src="images/headset.jpg" alt="" class="img" id="img4"></img>
+                <a href="#slider-span4" class="button" id="button-4"></a>
+            </div>
+            <div class="slides-div" id="slide-5">
+                <img src="images/headset.jpg" alt="" class="img" id="img5"></img>
+                <a href="#slider-span5" class="button" id="button-5"></a>
             </div>
         </div>
     </div>
