44f0f009dd779a8e89e14bdeb2be639754d1a061
Flayre <mariesamantha.f@gmail.com>
2023-05-24 23:55:07 +0700

    Modified server and password .env

--- website/src/server/mail.js
+++ website/src/server/mail.js
@@ -1,28 +1,32 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
-require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = 3001;
+
+const transporter = nodemailer.createTransport({
+  host: 'Gmail',
+  port: 587,
+  secure: true,
+  auth: {
+    user: 'personalwebsitemsf@gmail.com',
+    pass: process.env.GMAILPASS,
+  },
+});
 
 app.use(express.json());
 
 app.post('/send-email', (req, res) => {
   const { name, email, message } = req.body;
 
-  const transporter = nodemailer.createTransport({
-    service: 'Gmail', 
-    auth: {
-      user: 'personalwebsitemsf@gmail.com',
-      pass: process.env.GMAIL_PASSWORD,
-    },
-  });
-
   const mailOptions = {
-    from: email,
-    to: 'mariesamantha.f@gmail.com',
-    subject: 'New Website Form Submission',
-    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    to: 'mariesamantha.f@gmail.com', // Replace with the recipient's email address
+    subject: 'New Message from Your Website',
+    text: `
+      Name: ${name}
+      Email: ${email}
+      Message: ${message}
+    `,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
@@ -31,11 +35,11 @@ app.post('/send-email', (req, res) => {
       res.status(500).send('An error occurred while sending the email.');
     } else {
       console.log('Email sent:', info.response);
-      res.send('Email sent successfully!');
+      res.status(200).send('Email sent successfully.');
     }
   });
 });
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
