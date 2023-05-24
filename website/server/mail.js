62c0972eafbf1fc79cf523d799bda62ca00e1c08
Flayre <mariesamantha.f@gmail.com>
2023-05-24 23:09:10 +0700

    Installed server packages

--- website/server/mail.js
+++ website/server/mail.js
@@ -1,72 +1,40 @@
-const express = require("express");
-const nodemailer = require("nodemailer");
-const cors = require("cors");
-const uploadImage = require("./uploadImage.js");
+const express = require('express');
+const nodemailer = require('nodemailer');
+
 const app = express();
-const port = 5000;
+const port = 3000;
 
-app.use(cors());
-app.use(express.json({ limit: "25mb" }));
-app.use(express.urlencoded({ limit: "25mb" }));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-});
+app.use(express.json());
 
-var myemail = process.env.SENDER_EMAIL;
-var mypassword = process.env.APPLICATION_PASSWORD;
+app.post('/send-email', (req, res) => {
+  const { name, email, message } = req.body;
 
-function sendEmail({ recipient_email, OTP }) {
-  return new Promise((resolve, reject) => {
-    var transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: myemail,
-        pass: mypassword,
-      },
-    });
-
-    const mail_configs = {
-      from: myemail,
-      to: recipient_email,
-      subject: "Subject"
-    };
-    transporter.sendMail(mail_configs, function (error, info) {
-      if (error) {
-        console.log(error);
-        return reject({ message: `An error has occured` });
-      }
-      return resolve({ message: "Email sent succesfuly" });
-    });
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail', 
+    auth: {
+      user: 'personalwebsitemsf@gmail.com',
+      pass: 'Nottherealpassword',
+    },
   });
-}
 
-app.get("/", (req, res) => {
-  sendEmail()
-    .then((response) => res.send(response.message))
-    .catch((error) => res.status(500).send(error.message));
-});
+  const mailOptions = {
+    from: email,
+    to: 'mariesamantha.f@gmail.com',
+    subject: 'New Website Form Submission',
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
 
-app.post("/send_recovery_email", (req, res) => {
-  console.log("Somebody just hit me");
-  sendEmail(req.body)
-    .then((response) => res.send(response.message))
-    .catch((error) => res.status(500).send(error.message));
-});
-
-app.post("/uploadImage", (req, res) => {
-  uploadImage(req.body.image)
-    .then((url) => res.send(url))
-    .catch((err) => res.status(500).send(err));
-});
-
-app.post("/uploadMultipleImages", (req, res) => {
-  uploadImage
-    .uploadMultipleImages(req.body.images)
-    .then((urls) => res.send(urls))
-    .catch((err) => res.status(500).send(err));
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('An error occurred while sending the email.');
+    } else {
+      console.log('Email sent:', info.response);
+      res.send('Email sent successfully!');
+    }
+  });
 });
 
 app.listen(port, () => {
-  console.log(`nodemailerProject is listening at http://localhost:${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
