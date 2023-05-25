98a2e686024016e0dad82a5e2a687a5f1405c9d3
Flayre <mariesamantha.f@gmail.com>
2023-05-25 22:03:09 +0700

    Made contact form responsive

--- website/src/client/main.js
+++ website/src/client/main.js
@@ -1,9 +1,9 @@
 import './main.css';
 import './scroll.js';
-import React, { useRef , useState} from 'react';
+import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import { gsap } from 'gsap';
-import { CSSPlugin } from 'gsap/CSSPlugin'; // Import the CSSPlugin
+import { CSSPlugin } from 'gsap/CSSPlugin';
 
 gsap.registerPlugin(CSSPlugin); 
 
